66f6d0bf3f5150227f55ff1f8f3d2c1cc79e3d82
Leo Andersson Guillou <leo.anderssonguillo@learnet.se>
2020-10-15 11:12:50 +0200

    Working loop

--- Slagsmål Folder/Program.cs
+++ Slagsmål Folder/Program.cs
@@ -7,6 +7,7 @@ namespace Slagsmål
         static void Main(string[] args)
         {
             Random generator = new Random();
+            bool fighting = true;
             int wHP = 200;
             int gHP = 200;
             int wAttack = 0;
@@ -15,24 +16,30 @@ namespace Slagsmål
 
             Console.WriteLine("Walter VS Gorilla!");
 
-            Console.WriteLine("Round " + round + "!");
-            Console.WriteLine();
+            while (fighting == true)
+            {
+                Console.WriteLine("ROUND " + round + "!");
+                Console.WriteLine();
 
-            wAttack = generator.Next(1, 51);
-            Console.WriteLine("Walter attacks!");
-            Console.WriteLine(wAttack + " damage!");
-            gHP = gHP - wAttack;
-            Console.WriteLine();
+                wAttack = generator.Next(1, 51);
+                Console.WriteLine("Walter attacks!");
+                Console.WriteLine(wAttack + " damage!");
+                gHP = gHP - wAttack;
+                Console.WriteLine();
 
-            gAttack = generator.Next(1, 51);
-            Console.WriteLine("Gorilla attacks!");
-            Console.WriteLine(gAttack + " damage!");
-            wHP = wHP - gAttack;
-            Console.WriteLine();
+                gAttack = generator.Next(1, 51);
+                Console.WriteLine("Gorilla attacks!");
+                Console.WriteLine(gAttack + " damage!");
+                wHP = wHP - gAttack;
+                Console.WriteLine();
 
-            Console.WriteLine("Walter: " + wHP + " HP");
-            Console.WriteLine("Gorilla: " + gHP + " HP");
-            Console.ReadLine();
+                Console.WriteLine("Walter: " + wHP + " HP");
+                Console.WriteLine("Gorilla: " + gHP + " HP");
+                Console.WriteLine();
+                Console.WriteLine("----------------------------------");
+                Console.ReadLine();
+                round = round + 1;
+            }
         }
     }
 }
